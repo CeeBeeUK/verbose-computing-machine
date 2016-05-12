23e7709893ba1918794817b1ed22b9431e1bf9f7
CeeBeeUK <colinbruce@gmail.com>
2016-05-12 15:39:06 +0100

    Added long-form accessors to dog

--- chapter-2/animals.rb
+++ chapter-2/animals.rb
@@ -11,12 +11,32 @@ end
 
 class Dog
 	
+	def name=(value)
+		@name = value
+	end
+
+	def name
+		@name
+	end
+
+	def age=(value)
+		@age = value
+	end
+
+	def age
+		@age
+	end
+
 	def talk 
-		puts "Bark!"
+		puts "#{@name} says Bark!"
 	end
 	
 	def move(destination)
-		puts "Running to the #{destination}."
+		puts "#{@name} runs to the #{destination}."
+	end
+
+	def report_age
+		puts "#{@name} is #{@age} years old"
 	end
 end
 
@@ -32,10 +52,20 @@ class Cat
 end
 
 bird = Bird.new
-dog = Dog.new
 cat = Cat.new
 
 bird.move("tree")
-dog.talk
 bird.talk
 cat.move("house")
+
+fido = Dog.new
+fido.name = 'Fido'
+fido.age = 7
+fido.talk
+
+rex = Dog.new
+rex.name = "Rex"
+rex.age = 3
+rex.move("bed")
+rex.report_age
+fido.report_age
