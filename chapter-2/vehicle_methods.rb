21effb1242c39447bfbbd136a2e35cf3fd34bc08
CeeBeeUK <colinbruce@gmail.com>
2016-05-12 15:25:47 +0100

    created chapter 2 and added vehicles

---
+++ chapter-2/vehicle_methods.rb
@@ -0,0 +1,23 @@
+def accelerate
+	puts "Stepping on the gas"
+	puts "Speeding up"
+end
+
+def sound_horn
+	puts "Pressing the horn button"
+	puts "Beep beep!"
+end
+
+def use_headlights(brightness = "low-beam")
+	puts "Turning on the #{brightness} headlights"
+	puts "Watch out for deer!"
+end
+
+def mileage(miles_driven, gas_used)
+	miles_driven / gas_used
+end
+
+# sound_horn
+# accelerate
+# use_headlights
+# use_headlights("high-beam")
