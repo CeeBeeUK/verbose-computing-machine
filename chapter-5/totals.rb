96963d0c9c70e9bf91e40b07e03f416b7c8f9392
CeeBeeUK <colinbruce@gmail.com>
2016-05-15 12:49:41 +0100

    Refund added

--- chapter-5/totals.rb
+++ chapter-5/totals.rb
@@ -8,6 +8,18 @@ def total(prices)
 	amount
 end
 
+def refund(prices)
+	amount = 0
+	index = 0
+	while index < prices.length
+		amount -= prices[index]
+		index += 1
+	end
+	amount
+end
+
+
 prices = [3.99, 25.00, 8.99]
 
 puts format("%.2f", total(prices))
+puts format("%.2f", refund(prices))
