2c0ef440773d596cd3381759e7f858422e99fdd4
CeeBeeUK <colinbruce@gmail.com>
2016-05-15 12:52:11 +0100

    discount added

--- chapter-5/totals.rb
+++ chapter-5/totals.rb
@@ -18,8 +18,17 @@ def refund(prices)
 	amount
 end
 
+def show_discounts(prices)
+	index = 0
+	while index < prices.length
+		amount_off = prices[index] / 3.0
+		puts format("Your discount: £%.2f", amount_off)
+		index += 1
+	end
+end
 
 prices = [3.99, 25.00, 8.99]
 
 puts format("%.2f", total(prices))
 puts format("%.2f", refund(prices))
+show_discounts(prices)
