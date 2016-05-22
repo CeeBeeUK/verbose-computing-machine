70cf564e46bd1fcaa336f433090bbc7a9a24b026
CeeBeeUK <colinbruce@gmail.com>
2016-05-22 20:26:02 +0100

    switch to keyword arguments

--- chapter-7/candidate.rb
+++ chapter-7/candidate.rb
@@ -1,11 +1,11 @@
 class Candidate
 	attr_accessor :name, :age, :occupation, :hobby, :birthplace
-	def initialize(name, options = {})
+	def initialize(name, age: nil, occupation: nil, hobby: nil, birthplace: 'Sleepy Creek')
 		self.name = name
-		self.age = options[:age]
-		self.occupation = options[:occupation]
-		self.hobby = options[:hobby]
-		self.birthplace = options[:birthplace]
+		self.age = age
+		self.occupation = occupation
+		self.hobby = hobby
+		self.birthplace = birthplace
 	end
 end
 
@@ -17,5 +17,5 @@ def print_summary(candidate)
 	puts "Birthplace: #{candidate.birthplace}"
 end
 
-p Candidate.new('Carl Barnes')
 p Candidate.new('Carl Barnes', occupation: 'Attorney')
+p Candidate.new('Amy Nguyen', age: 37, occupation: 'Engineer')
