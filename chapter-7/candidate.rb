537122a9d09e32e333db2184b857a6cdb248e9ad
CeeBeeUK <colinbruce@gmail.com>
2016-05-22 18:49:54 +0100

    corrected name setter

--- chapter-7/candidate.rb
+++ chapter-7/candidate.rb
@@ -1,7 +1,7 @@
 class Candidate
 	attr_accessor :name, :age, :occupation, :hobby, :birthplace
 	def initialize(name, options = {})
-		self.name = options[:name]
+		self.name = name
 		self.age = options[:age]
 		self.occupation = options[:occupation]
 		self.hobby = options[:hobby]
