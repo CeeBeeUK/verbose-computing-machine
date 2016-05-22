6ef0105bf0d33a43b8b457426c54ab5f66fbc91e
CeeBeeUK <colinbruce@gmail.com>
2016-05-22 18:18:28 +0100

    Added candidate

---
+++ chapter-7/canditate.rb
@@ -0,0 +1,18 @@
+class Candidate
+	attr_accessor :name, :age, :occupation, :hobby, :birthplace
+	def initialize(name, age, occupation, hobby, birthplace)
+		self.name = name
+		self.age = age
+		self.occupation = occupation
+		self.hobby = hobby
+		self.birthplace = birthplace
+	end
+end
+
+def print_summary(candidate)
+	puts "Candidate: #{candidate.name}"	
+	puts "Age: #{candidate.age}"
+	puts "Occupation: #{candidate.occupation}"
+	puts "Hobby: #{candidate.hobby}"
+	puts "Birthplace: #{candidate.birthplace}"
+end
