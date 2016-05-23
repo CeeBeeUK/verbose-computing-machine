33611f5a126808dfb54281f4a84805442cb1411f
CeeBeeUK <colinbruce@gmail.com>
2016-05-23 22:04:22 +0100

    Updated to hash default

--- chapter-8/celestial_body.rb
+++ chapter-8/celestial_body.rb
@@ -2,14 +2,29 @@ class CelestialBody
 	attr_accessor :type, :name
 end
 
+default_body = CelestialBody.new
+default_body.type = 'planet'
+bodies = Hash.new(default_body)
+
+p bodies.default
+bodies['Mars'].name = 'Mars'
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'Moon'
+bodies['Venus'].name = 'Venus'
+
+p bodies.default
+p bodies
+
 altair = CelestialBody.new
 altair.name = 'Altair'
 altair.type = 'star'
 
-polaris = altair
+polaris = CelestialBody.new
 polaris.name = 'Polaris'
+polaris.type = 'star'
 
-vega = polaris
+vega = CelestialBody.new
 vega.name = 'Vega'
+vega.type = 'star'
 
 puts altair.name, polaris.name, vega.name
