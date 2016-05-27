729e34c7c792c85df173a6ccefc22ea1c3643633
CeeBeeUK <colinbruce@gmail.com>
2016-05-27 19:20:00 +0100

    switched to spaceship operator

--- chapter-10/steak.rb
+++ chapter-10/steak.rb
@@ -2,8 +2,14 @@ class Steak
 	attr_accessor :grade
 	GRADE_SCORES = { prime: 3, choice: 2, select:1 }
 
-	def > (other)
-		GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+	def <=> (other)
+		if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+			return -1
+		elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+			return 0
+		else
+			return 1
+		end
 	end
 end
 
@@ -13,6 +19,5 @@ first_steak.grade = :prime
 second_steak = Steak.new
 second_steak.grade = :choice
 
-if first_steak > second_steak
-	puts "I'll take #{first_steak.inspect}."
-end
+puts first_steak <=> second_steak
+puts second_steak <=> first_steak
