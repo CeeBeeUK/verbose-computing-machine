27393ad5587ffe0a7f5a138503e4808829bed388
CeeBeeUK <colinbruce@gmail.com>
2016-05-29 17:03:31 +0100

    added descriptions and generated documentation

--- chapter-11/word_splitter.rb
+++ chapter-11/word_splitter.rb
@@ -1,9 +1,14 @@
+# This class allows you to perform various functions
+# on the words in a string.
 class	WordSplitter
 
 	include Enumerable
 
+	# The string to split into words
 	attr_accessor :string
 
+	# Passes each word in the string into a block,
+	# one at a time.
 	def each 
 		string.split(" ").each do |word|
 			yield word
@@ -11,20 +16,3 @@ class	WordSplitter
 	end
 
 end
-
-splitter = WordSplitter.new
-splitter.string = 'one two three four'
-
-splitter.each { |word| puts word }
-
-splitter = WordSplitter.new
-splitter.string = 'how do you do'
-
-p splitter.find_all { |word| word.include?('d') }
-p splitter.reject { |word| word.include?('d') }
-p splitter.map { |word| word.reverse }
-
-p splitter.any? { |word| word.include?('e') }
-p splitter.count
-p splitter.first
-p splitter.sort
