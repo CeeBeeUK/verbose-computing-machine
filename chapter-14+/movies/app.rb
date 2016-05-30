a8552c3ad082fedb3edf04ce86695ab30ee50a62
CeeBeeUK <colinbruce@gmail.com>
2016-05-30 22:11:57 +0100

    Add show route and handler

--- chapter-14+/movies/app.rb
+++ chapter-14+/movies/app.rb
@@ -5,13 +5,7 @@ require 'movie_store'
 store = MovieStore.new('movies.yml')
 
 get('/movies') do
-	@movies = []
-	@movies[0] = Movie.new
-	@movies[0].title = 'Jaws'
-	@movies[1] = Movie.new
-	@movies[1].title = 'Alien'
-	@movies[2] = Movie.new
-	@movies[2].title = 'Terminator 2'
+	@movies = store.all
 	erb :index
 end
 
@@ -26,4 +20,10 @@ post('/movies/create') do
 	@movie.year = params['year']
 	store.save(@movie)
 	redirect '/movies/new'
+end
+
+get('/movies/:id') do
+	id = params['id'].to_i
+	@movie = store.find(id)
+	erb :show
 end
--- chapter-14+/movies/lib/movie_store.rb
+++ chapter-14+/movies/lib/movie_store.rb
@@ -6,6 +6,12 @@ class MovieStore
 		@store = YAML::Store.new(file_name)
 	end
 
+	def find(id)
+		@store.transaction do
+			@store[id]
+		end
+	end
+
 	def save(movie)
 		@store.transaction do
 			unless movie.id
@@ -14,5 +20,11 @@ class MovieStore
 			end
 			@store[movie.id] = movie
 		end	
+	end
+
+	def all
+		@store.transaction do
+			@store.roots.map { |id| @store[id] }
+		end
 	end
 end
